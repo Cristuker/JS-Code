b3e970f22934e44766f341d4bac92af548322286
Cristuker <cristian123105@gmail.com>
2019-07-04 16:08:51 +0000

    saltos e laços

---
+++ JS-Daciuk/laços.js
@@ -0,0 +1,24 @@
+//do while
+var cont = 1
+console.log('===== do - while =====')
+do{
+    console.log( cont ++)
+}while( cont < 10);
+console.log('===== For ====')
+for(var i =0; i <=10; i++){
+    console.log(i)
+}
+//for
+console.log('==== for in ====')
+var car ={
+    brand: 'VW',
+    model: 'gol',
+    year: 2013
+};
+// for in serve para percorrer propriedades de objetos e é mais lento que o for normal
+for(var prop in car){
+    console.log('props de carro '+car[prop])
+}
+//=====outra forma de usar o in
+console.log('year in car?', 'year' in car)
+console.log('doors in car?', 'doors' in car)
---
+++ JS-Daciuk/saltos.js
@@ -0,0 +1,25 @@
+/*Saltos
+Pular partes em códigos
+-----------------
+return é um salto = pq vc pode usar ele no meio da função e assim pular instruções
+---
+break = também é uma instrução de salto ele para e pula para fora do laço 
+---
+continue = o continue n executa nada q vem pela frente e volta pro inicio do laço por ex
+*/
+ /*for(var i = 0; i <=10 ; i++){
+     console.log(i)
+     if(i === 5 ){
+         break;
+     }
+ }*/
+ //---------------- 
+ var arr = [1,2,3,4,5,6,7,8,9,10]
+ for(var i = 0; i <=arr.length ; i++){
+     
+     if(i === 5 ){
+         continue;//basicamente o continue n executa nada q vem pela frente console.log por exemplo e volta pro inicio do for, no caso ele puxa a exibição do número 5
+     }
+     console.log(i)
+ }
+ console.log('fim')
