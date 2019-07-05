9208da9e23d084b0393a21c81a1367206fd8b600
Cristuker <cristian123105@gmail.com>
2019-07-05 17:10:44 +0000

    objetos.js

--- JS-Daciuk/objetos.js
+++ JS-Daciuk/objetos.js
@@ -19,3 +19,12 @@ console.log(obj+''+obj2)
 var objCopy = obj
 objCopy === obj //retorna true por ter uma referencia de obj, se eu mudar algum dos dois objetos o outro muda automaticamente
 //por ser referencia
+
+//Crianção de objetos=========
+/*
+Literais var obj = {}
+Como construtor (new) var newObj = new Object(); // Criar com literal ou construtor tanto faz, eles fazem a mesma coisa
+com Object.create()
+
+Object.prototyoe = prototipo do objeto que é criado, todo objeto que é criado ele herda do objeto prototipo
+*/ 
