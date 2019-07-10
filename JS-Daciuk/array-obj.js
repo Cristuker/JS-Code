255f6c91072659d85e60c8f6ffb17939020d558a
Cristuker <cristian123105@gmail.com>
2019-07-10 14:28:21 -0300

    array

--- JS-Daciuk/array-obj.js
+++ JS-Daciuk/array-obj.js
@@ -1,1 +1,10 @@
-//.pop() remove item do final do array
+/*
+.pop() -> remove item do final do array
+.join() -> junta os itens do array em uma string, cada item separado por virgula -> se eu passar algo por parametro, esse sera o separador
+que por padrão é a virgula
+.reverse() -> ele altera a ordem do array e caso queira que ele volte ao normal o reverse() tem que ser usado novamente
+.sort() -> inverte o array principal / ordena por ordem alfabetica 
+
+
+
+*/
