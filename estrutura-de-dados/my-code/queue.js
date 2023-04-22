8d6c49933a779057f5715e88e3213861188e9d11
Cristian silva <cristian123105@gmail.com>
2023-04-22 14:27:44 -0300

    wip

--- estrutura-de-dados/my-code/queue.js
+++ estrutura-de-dados/my-code/queue.js
@@ -1,17 +1,26 @@
 class Queue {
+    
+    // Quantidade de items na fila.
     #count = 0;
-    #lowestCount = 0;
-    #items = {};
 
+    // Serve para controlar quando é o primeiro item da fila. Não necessáriamente pode ser 0.
+    #lowestCount = 0;
+
+    // Objeto com todos os items armazenados.
+    #items = {};
+    
+    // Adiciona os items na fila.
     enqueue(element) {
         this.#items[this.#count] = element;
         this.#count++;
     }
 
+    // Verifica se a fila está vazia.
     isEmpty() {
         return (this.#count - this.#lowestCount) === 0;
     }
 
+    // Remove o primeiro item da fila.
     dequeue() {
         if (this.isEmpty()) {
             return undefined;
@@ -23,6 +32,7 @@ class Queue {
         return result;
     }
 
+    // Retorna o primeiro item da fila.
     peek() {
         if(this.isEmpty()) {
             return ''
@@ -31,16 +41,19 @@ class Queue {
         return this.#items[this.#lowestCount];
     }
 
+    // Retorna o tamanho da fila.
     size() {
         return this.#count - this.#lowestCount;
     }
 
+    // Limpa a fila.
     clear() {
         this.#items = {};
         this.#count = 0;
         this.#lowestCount = 0;
     }
 
+    // Retorna todos os items da fila em uma string.
     toString() {
         if (this.isEmpty()) {
             return '';
@@ -62,16 +75,12 @@ module.exports = {
 
 const queue = new Queue();
 
-// console.log(queue.isEmpty());
-
-// queue.enqueue('John');
-// queue.enqueue('Jack');
-// queue.enqueue('Camila');
-// console.log(queue.toString())
-// console.log(queue.size());
-// console.log(queue.isEmpty());
-
-// queue.dequeue();
-// console.log(queue.toString());
-// queue.dequeue();
-// console.log(queue.toString());
+queue.enqueue("Cristian"); // Cristian
+queue.enqueue("Paulo"); // Cristian, Paulo
+console.log(queue.size()) // 2
+queue.enqueue("Mateus"); // Cristian, Paulo, Mateus
+queue.dequeue();
+console.log(queue.toString()) // Paulo, Mateus
+console.log(queue.peek()); // Paulo
+queue.clear();
+console.log(queue.isEmpty()); // True
